4c57b3a45b6e3b679bc08503093ca716ef6f7d04
Krystian Madra <madrakrystian@gmail.com>
2018-12-16 19:08:32 +0100

    Update GuitarDao

--- Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/GuitarDao.java
+++ Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/GuitarDao.java
@@ -3,54 +3,50 @@ package com.github.krystianmadra.guitarshop;
 import com.github.krystianmadra.guitarshop.entities.GuitarEntity;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.*;
 
-@Startup
-@Singleton
+@Stateless
 public class GuitarDao {
 
     private final Set<GuitarEntity> guitars = new HashSet<>();
 
+    @PersistenceContext(unitName = "primary")
+    protected EntityManager em;
+
     public GuitarDao() {
     }
 
-
-    @PostConstruct
-    void init(){
-        guitars.add(new GuitarEntity(1l, "Ibanez 740", 1199.99));
-        guitars.add(new GuitarEntity(2l, "Yamaha C30M", 530.0));
-        guitars.add(new GuitarEntity(3l, "Yamaha F310P", 730.99));
-    }
-
     public Optional<GuitarEntity> getGuitarById(long id){
-        return guitars.stream().filter(e -> e.getId() == id).findFirst();
+        TypedQuery<GuitarEntity> query = em.createQuery("select new GuitarEntity(g.id) from GuitarEntity g where g.id = :id", GuitarEntity.class);
+        query.setParameter("id", id);
+
+        GuitarEntity ret = query.getSingleResult();
+
+        return Optional.of(ret);
     }
 
     public List<GuitarEntity> getAll(){
-        return new ArrayList(guitars);
+        return em.createQuery("select g from GuitarEntity g", GuitarEntity.class).getResultList();
     }
 
-    public GuitarEntity addGuitar(GuitarEntity guitar){
-        guitar.setId((long) (guitars.size()+1));
-        guitars.add(guitar);
+    public GuitarEntity addGuitar(GuitarEntity guitar) {
+        em.persist(guitar);
         return guitar;
-    }
-
-    public GuitarEntity editGuitar(GuitarEntity guitar){
-        GuitarEntity fromDB = guitars.stream().filter(e -> e.getId() == guitar.getId()).findFirst().get();
-        fromDB.update(guitar);
-        return fromDB;
     }
 
     public void remove(long id) {
         guitars.remove(new GuitarEntity(id));
     }
 
-    public GuitarEntity update(GuitarEntity guitar) {
-        Optional<GuitarEntity> guitarFromDatabase = guitars.stream().filter(ent -> ent.getId().equals(guitar.getId())).findFirst();
-        guitarFromDatabase.ifPresent(fromDb -> fromDb.update(guitar));
-        return guitarFromDatabase.get();
+    public GuitarEntity update(GuitarEntity guitarEntity) {
+        GuitarEntity dbGuitar = getGuitarById(guitarEntity.getId()).get();
+        dbGuitar.update(guitarEntity);
+        GuitarEntity ret = em.merge(guitarEntity);
+        return ret;
     }
+
 }
