a6e553e7a48ed95beedcae1aecaa26e1edfd8c8d
verbatim144 <vlad061998@gmail.com>
2019-01-18 21:58:27 +0100

    Update guitar services

--- Shop-Frontend/GuitarStore/src/app/guitars/guitar.ts
+++ Shop-Frontend/GuitarStore/src/app/guitars/guitar.ts
@@ -1,9 +1,10 @@
 import {Category} from '../categories/category';
 
 export class Guitar {
+  id: number;
   name: string;
   price: number;
   description: string;
-  category: Category;
-  amount: number;
+  category: string;
+  quantity: number;
 }
