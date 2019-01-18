9b4eb16043ff3590742cd34cca3bb1a10842dca0
Krystian Madra <madrakrystian@gmail.com>
2019-01-18 15:10:57 +0100

    Update GuitarDao

--- Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/GuitarDao.java
+++ Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/GuitarDao.java
@@ -17,6 +17,18 @@ public class GuitarDao {
     public GuitarDao() {
     }
 
+    public void remove(long id) {
+        GuitarEntity dbGuitar = getGuitarById(id).get();
+        em.remove(dbGuitar);
+    }
+
+    public GuitarEntity update(GuitarEntity guitarEntity) {
+        GuitarEntity dbGuitar = getGuitarById(guitarEntity.getId()).get();
+        dbGuitar.update(guitarEntity);
+        GuitarEntity ret = em.merge(dbGuitar);
+        return ret;
+    }
+
     public Optional<GuitarEntity> getGuitarById(long id){
         TypedQuery<GuitarEntity> query = em.createQuery("select g from GuitarEntity g where g.id = :id", GuitarEntity.class);
         query.setParameter("id", id);
@@ -33,7 +45,6 @@ public class GuitarDao {
         return Optional.of(ret);
     }
 
-
     public List<GuitarEntity> getAll(){
         return em.createQuery("select g from GuitarEntity g", GuitarEntity.class).getResultList();
     }
@@ -41,22 +52,5 @@ public class GuitarDao {
     public GuitarEntity addGuitar(GuitarEntity guitar) {
         em.persist(guitar);
         return guitar;
-    }
-
-    public void remove(long id) {
-        GuitarEntity dbGuitar = getGuitarById(id).get();
-        em.remove(dbGuitar);
-    }
-
-    public GuitarEntity update(GuitarEntity guitarEntity) {
-        /* tez dziala
-        Optional<GuitarEntity> dbGuitar = getAll().stream().filter(ent -> ent.getId().equals(guitarEntity.getId())).findFirst();
-        dbGuitar.ifPresent(fromDb -> fromDb.update(guitarEntity));
-        return dbGuitar.get();
-        */
-        GuitarEntity dbGuitar = getGuitarById(guitarEntity.getId()).get();
-        dbGuitar.update(guitarEntity);
-        GuitarEntity ret = em.merge(dbGuitar);
-        return ret;
     }
 }
