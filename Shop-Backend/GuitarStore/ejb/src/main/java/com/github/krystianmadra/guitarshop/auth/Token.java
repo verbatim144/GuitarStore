ed50afb2f668dff7dc2e151289b9ca8c79cb39b5
Krystian Madra <madrakrystian@gmail.com>
2019-01-23 21:28:00 +0100

    Update Token

--- Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/auth/Token.java
+++ Shop-Backend/GuitarStore/ejb/src/main/java/com/github/krystianmadra/guitarshop/auth/Token.java
@@ -1,11 +1,10 @@
 package com.github.krystianmadra.guitarshop.auth;
 
+import com.github.krystianmadra.guitarshop.entities.Role;
+
 import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.time.Instant;
 import java.time.LocalTime;
-import java.util.Date;
-import java.util.Random;
 
 public class Token {
 
@@ -13,15 +12,17 @@ public class Token {
     private String token;
     private LocalTime expirationDate;
     private Long userId;
+    private Role userRole;
 
-    public Token(Long userId) {
-        this(userId, new BigInteger(130, random).toString(32));
+    public Token(Long userId, Role role) {
+        this(userId, role, new BigInteger(130, random).toString(32));
         this.expirationDate = LocalTime.now().plusMinutes(10).plusHours(1);
     }
 
-    public Token(Long userId, String token) {
+    public Token(Long userId, Role role, String token) {
         this.userId = userId;
         this.token = token;
+        this.userRole = role;
         this.expirationDate = LocalTime.now().plusMinutes(10);
     }
 
@@ -47,5 +48,17 @@ public class Token {
 
     public void setUserId(Long userId) {
         this.userId = userId;
+    }
+
+    public static SecureRandom getRandom() {
+        return random;
+    }
+
+    public Role getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Role userRole) {
+        this.userRole = userRole;
     }
 }
