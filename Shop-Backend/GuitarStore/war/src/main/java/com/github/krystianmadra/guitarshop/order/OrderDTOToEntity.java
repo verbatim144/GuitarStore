340726238b7f318356c706b88c831bc6137c26ec
Krystian Madra <madrakrystian@gmail.com>
2019-01-30 12:54:37 +0100

    Update OrderDtos

--- Shop-Backend/GuitarStore/war/src/main/java/com/github/krystianmadra/guitarshop/order/OrderDTOToEntity.java
+++ Shop-Backend/GuitarStore/war/src/main/java/com/github/krystianmadra/guitarshop/order/OrderDTOToEntity.java
@@ -1,24 +1,22 @@
 package com.github.krystianmadra.guitarshop.order;
 
-import com.github.krystianmadra.guitarshop.entities.OrderDetails;
+import com.github.krystianmadra.guitarshop.entities.GuitarEntity;
 import com.github.krystianmadra.guitarshop.entities.OrderEntity;
 import com.github.krystianmadra.guitarshop.entities.UserEntity;
 import com.github.krystianmadra.guitarshop.guitar.GuitarDTOToEntity;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OrderDTOToEntity {
 
     public static OrderEntity toEntity(OrderDTO dto){
         OrderEntity ret = new OrderEntity();
-        ret.setId(dto.getId());
         ret.setUser(new UserEntity(dto.getUser()));
-        ret.setOrderDate(dto.getOrderDate());
-        ret.setGuitars(new ArrayList<>());
-
-        //List<OrderDetails> orderDetails = dto.getOrdersDetails().stream().map(OrderDetailsDTOToEntity::toEntity).collect(Collectors.toList());
-        //ret.setOrdersDetails(orderDetails);
+        ret.setTotalPrice(dto.getTotalPrice());
+        Set<GuitarEntity> guitars = dto.getGuitars().stream().map(GuitarDTOToEntity::toEntity).collect(Collectors.toSet());
+        ret.setGuitars(guitars);
 
         return ret;
     }
