573fad7695cbaa28c6863e946ac6179edd042472
verbatim144 <vlad061998@gmail.com>
2019-01-30 21:13:15 +0100

    Update classic component fucntionality

--- Shop-Frontend/GuitarStore/src/app/classic/classic.component.ts
+++ Shop-Frontend/GuitarStore/src/app/classic/classic.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Guitar} from '../guitars/guitar';
 import {GuitarsService} from '../guitars/guitars.service';
 import * as moment from 'moment';
+import {OrderService} from '../order-service/order.service';
 
 @Component({
   selector: 'app-classic',
@@ -10,9 +11,10 @@ import * as moment from 'moment';
 })
 export class ClassicComponent implements OnInit {
 
-  guitars : Guitar[];
+  guitars: Guitar[];
+  guitarFind = new Guitar;
 
-  constructor(private guitarService: GuitarsService) { }
+  constructor(private guitarService: GuitarsService, private orderService: OrderService) { }
 
   ngOnInit() {
     this.getGuitars();
@@ -29,9 +31,9 @@ export class ClassicComponent implements OnInit {
   }
 
   public isLoggedIn() {
-    if(localStorage.getItem('userToken')==null){
+    if (localStorage.getItem('userToken') === null) {
       return true;
-    }else{
+    } else {
       return moment().isBefore(this.getExpiration());
     }
   }
@@ -40,5 +42,22 @@ export class ClassicComponent implements OnInit {
     const expiration = localStorage.getItem('expirationDate');
     const expiresAt = JSON.parse(expiration);
     return moment(expiresAt);
+  }
+
+  getGuitarById(id: number) {
+    return this.guitarService.getGuitarById(id)
+      .subscribe(
+        guitar => {
+          this.guitarFind = guitar;
+          this.orderService.addGuitar(this.guitarFind);
+          /*localStorage.setItem('order', JSON.stringify(this.guitarFind));*/
+
+        }
+      );
+  }
+
+  orderClick(id: number) {
+    this.getGuitarById(id);
+
   }
 }
