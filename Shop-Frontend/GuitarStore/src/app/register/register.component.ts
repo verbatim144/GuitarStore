782aec36628bdc313d667007a520a6c70f2ce046
verbatim144 <vlad061998@gmail.com>
2019-01-31 07:50:21 +0100

    Add validation on the registration page

--- Shop-Frontend/GuitarStore/src/app/register/register.component.ts
+++ Shop-Frontend/GuitarStore/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {RegistrationService} from '../register-service/registration.service';
 import {User} from '../user/user';
 import {Guitar} from '../guitars/guitar';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +12,20 @@ import {Guitar} from '../guitars/guitar';
 })
 export class RegisterComponent implements OnInit {
 
+  model: any = {};
   userRegister= new User();
+  registerForm: FormGroup;
   submitted: false;
 
-  constructor(private registrationService: RegistrationService) { }
+  constructor(private registrationService: RegistrationService, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
   }
 
   createNewUser(){
     this.create();
+    alert('Zostałeś zarejestrowany!\n\n');
+    this.router.navigate(['/login']);
   }
 
 
